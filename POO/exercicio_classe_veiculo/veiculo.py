e6c5a334d3008db29a54bfeb680e48e24134af11
Thiago de Sousa Santos <129019650+thiago21sousa21@users.noreply.github.com>
2025-05-09 19:59:21 -0300

    inicio atividade POO classe carro

---
+++ POO/exercicio_classe_veiculo/veiculo.py
@@ -0,0 +1,17 @@
+import re
+
+class veiculo:
+  def __init__(self, chassi, marca, modelo, ano, cor, placa=None, proprietario=None, quilometragem=None, valor = None):
+    self.chassi = chassi
+    self.marca = marca
+    self.modelo = modelo
+    self.ano = ano
+    self.cor = cor
+    self.placa = placa
+    self.proprietario = proprietario
+    self.quilometragem = quilometrage
+    self.valor = valor 
+
+  def validar_placa(self, placa):
+    padrao = r'^[A-Z]{3}-[A-Z][A-Z0-9][0-9]{2}$'
+    return re.match(padrao, placa.upper().strip())
