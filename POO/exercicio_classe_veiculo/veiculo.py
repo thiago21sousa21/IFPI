15086c905d0ec8d65c5b0ef90ade588cee67a400
Thiago de Sousa Santos <129019650+thiago21sousa21@users.noreply.github.com>
2025-05-11 20:39:31 -0300

    POO exercicio classe veiculo adicionando metodo calcular_depreciação

--- POO/exercicio_classe_veiculo/veiculo.py
+++ POO/exercicio_classe_veiculo/veiculo.py
@@ -19,7 +19,25 @@ class veiculo:
 
 def validar_placa(placa):
     padrao = r'^[A-Z]{3}-?[0-9]{4}$|^[A-Z]-?\d[A-Z]\d{2}$'
-    return re.match(padrao, placa.upper().strip()) is not None
+    return re.match(padrao, placa.upper().strip()) is not 
+    
+def calcular_depreciacao(self):
+  # o Calcula a depreciação do veículo com base no ano de fabricação e no valor de
+  # mercado.
+  ano_atual = date.today().year
+  depreciacao = 5/100
+  ano_carro = self.ano
+  valor_depreciado = self.valor * (1 - depreciacao)**((ano_atual-ano_carro) +1)
+  valor_minimo = self.valor * (10/100)
+  if (ano_carro > ano_atual) :
+    print(f"Tu é viajante do tempo? Ajeita o ano desse carro aí, estamos em {ano_atual}")
+  else:
+    if valor_depreciado < valor_minimo:
+      print(valor_minimo)
+      return valor_minimo
+    else:
+      print(valor_depreciado)
+      return valor_depreciado
 
 v = validar_placa('nia8020')
 print(v)
