7fd2fc70e39dbcc35e17679a439b487a302cfe69
thiago21sousa21 <thiago21sousa21@gmail.com>
2025-05-27 14:25:17 -0300

    poo: inicio exercicio boleto bancario

---
+++ POO/exercicio_boleto_bancario/Boleto.py
@@ -0,0 +1,111 @@
+# Exercício de Revisão – Boleto Bancário
+
+# Utilizando o processo de abstração, implemente uma classe em Python que represente um
+# boleto bancário. Identifique atributos mutáveis e imutáveis, implemente um construtor da
+# classe e métodos para manipulação dos atributos mutáveis. Faça todas as validações
+# necessárias. Utilize encapsulamento e decoradores de leitura/escrita. Crie objetos para testar
+# os métodos implementados.
+
+from datetime import datetime, timedelta
+
+class Boleto:
+    __numero = 0
+    __status_disponiveis = ["em aberto", "pago", "vencido", "cancelado" ]
+    def __init__ (self, valor_original:float, data_pagamento:str):
+        self.__numero = Boleto.__numero
+        self.__nome_do_pagador = ""
+        self.__valor_original = float(valor_original)
+        self.__data_emissao = datetime.now()
+        self.__data_pagamento = self.__verificar_dia_vencimento(datetime.strptime(data_pagamento, "%d-%m-%Y"))
+        self.__status_boleto = Boleto.__status_disponiveis[0]
+        self.__valor_final  = None
+
+    def __str__(self):
+        return f"""numero: {self.numero}
+nome do pagador: {self.nome_do_pagador}
+valor original: {self.valor_original}
+data emissão: {self.data_emissao}
+data pagamento: {self.data_pagamento}
+status do boleto: {self.status_boleto}
+valor final: {self.valor_final}"""
+    
+    @property
+    def numero(self):
+        return self.__numero
+    
+    @property
+    def nome_do_pagador(self):
+        return self.__nome_do_pagador
+    
+    @property
+    def valor_original(self):
+        return self.__valor_original
+
+    @property
+    def data_emissao(self):
+        return self.__data_emissao
+    
+    @property
+    def data_pagamento(self):
+        return self.__data_pagamento
+    
+    @property
+    def status_boleto(self):
+        return self.__status_boleto
+
+    @property
+    def valor_final(self):
+        return self.__valor_final
+    
+    def __registrar_pagamento(self):
+        pass
+    
+    def __verificar_dia_vencimento(self, data_pagamento = datetime.now()):
+        if data_pagamento < self.data_emissao.date():
+            raise "A data de vencimento não pode ser maior que a dada de emissão!"
+
+    
+    
+
+teste = Boleto(100, "10-05-2020")
+print(teste)
+
+# Especificações:
+# 1. Atributos:
+# • Número do boleto (gerado automaticamente e único)
+# • Nome do pagador
+# • Valor original
+# • Data de emissão (registrada automaticamente na criação do boleto) //Não pode ser
+# uma data menor que a data atual
+# • Data de vencimento (definida no momento da criação)
+# • Data de pagamento (registrada quando pago) // Inicialmente: None
+# • Status do boleto: "em aberto", "pago", "vencido", “cancelado” // Inicialmente: “em
+# aberto”
+# • Valor final com juros e/ou desconto // inicialmente: 0,00
+
+# 2. Métodos sugeridos:
+# • Um método para registrar o pagamento:
+# o Define a data de pagamento
+# o Calcula o valor final (com desconto ou juros)
+# o Altera o status para "pago" ou "vencido" conforme a data
+# • Um método para consultar o valor atualizado:
+# o Considera:
+# ▪ Desconto de 5% se pago até 3 dias antes do vencimento
+# ▪ Multa de 2% + juros de 0,03% ao dia se pago após o vencimento
+# • Um método para cancelar o boleto, que altera o status do boleto para “cancelado”.
+# Não pode cancelar se o boleto já foi pago.
+
+# Regras de Validação:
+# • O número do boleto deve ser único
+# • O valor original deve ser positivo
+# • A data de vencimento deve ser posterior à emissão
+# • A data de pagamento não pode ser anterior à emissão
+# • O status só pode ser "pago" se o pagamento for registrado
+# • O status deve mudar para "vencido" se a data atual ultrapassar o vencimento e o
+# boleto ainda não tiver sido pago
+# • Boleto com o status “pago” não pode ser pago novamente.
+
+# Teste:
+# • Crie pelo menos três objetos da classe, com diferentes datas de vencimento e simule
+# pagamentos antecipados, pontuais e atrasados.
+# • Mostre
