babdb718b8409bfe93153ac6bac5beb9fa4f659e
Thiago Sousa <thiago21sousa21@gmail.com>
2025-10-20 10:40:58 -0300

    adicionando diagrama

--- topicos_especiais/avaliacao/av1/startup/main.py
+++ topicos_especiais/avaliacao/av1/startup/main.py
@@ -1,14 +1,6 @@
 import os
 from utils import limpar_terminal, limpar_banco
-from tests.usuario_test import Testar_usuario 
-from tests.posts_tests import Testar_posts
-from tests.comentarios_tests import Testar_comentarios
-from tests.telefone_teste import Testar_telefones
-from tests.like_tests import Testar_likes
-from tests.amizade_tests import Testar_amizade
-
-
-
+from tests import Testar_amizade, Testar_comentarios, Testar_likes, Testar_posts, Testar_telefones, Testar_usuario
 
 def limpar_geral():
     limpar_banco()
---
+++ topicos_especiais/avaliacao/av1/startup/tests/__init__.py
@@ -0,0 +1,7 @@
+from tests.usuario_test import Testar_usuario 
+from tests.posts_tests import Testar_posts
+from tests.comentarios_tests import Testar_comentarios
+from tests.telefone_teste import Testar_telefones
+from tests.like_tests import Testar_likes
+from tests.amizade_tests import Testar_amizade
+
